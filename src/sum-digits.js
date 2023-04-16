30a185362a2c565f1b2ff084def6b63125e896d0
OlegPetin <olegpetin87@gmail.com>
2023-04-17 00:47:59 +0300

    sort-by-height

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -14,7 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function getSumOfDigits(n) {
   if (n < 10) return n;
-  return getSumOfDigits(n % 10 + getSumOfDigits(Math.floor(n / 10)))
+  return getSumOfDigits(n % 10 + getSumOfDigits(Math.floor(n / 10)));
 }
 
 module.exports = {
